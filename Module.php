65a0fdd3892626fd88c2c846c2ef142e586270b4
Gian Nunes <gian@btitecnologia.com>
2016-05-03 21:32:06 -0300

    LogBug basic

---
+++ Module.php
@@ -0,0 +1,66 @@
+<?php
+namespace LogBug;
+
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        include __DIR__ . '/src/Service/error.php';
+        
+        $application        =   $e->getTarget();
+        $eventManager       =   $application->getEventManager();
+        $services           =   $application->getServiceManager();
+
+
+        //$service            =   $services->get('BugsnagServiceException');
+        // Register the PHP exception and error handlers
+        //$service->setupErrorHandlers();
+ 
+
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, function ($event) use ($services) {
+           $exception      =   $event->getParam('exception');
+            echo 'erro';
+            exit();
+            // No exception, stop the script
+            if (!$exception) return;
+
+            
+            echo 'erro';
+            exit();
+           // $service->sendException($exception);
+        });
+    }
+    
+
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ],
+            ],
+        ];
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                'BugsnagServiceException' =>  function($sm) {
+                    $options = $sm->get('ZfBugsnag\Options\BugsnagOptions');
+                    $service = new \ZfBugsnag\Service\BugsnagService($options);
+                    return $service;
+                },
+            ],
+        ];
+    }
+}
---
+++ config/module.config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'controllers' => [
+        'invokables' => [
+           // 'ZfBugsnag\Service\Bugsnag' => 'ZfBugsnag\Service\BugsnagService',
+        ],
+    ],
+    'service_manager' => [
+        'factories' => [
+           // 'ZfBugsnag\Options\BugsnagOptions' => 'ZfBugsnag\Options\BugsnagOptionsFactory',
+        ],
+    ],
+];
